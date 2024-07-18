5f258a70ff6ab8abe6242af98e94c5b5c43d8ef7
Andrea Meroni <andrea.meroni.personal@gmail.com>
2024-07-18 13:22:41 +0200

    chore(release): 1.0.9

--- .projenrc.py
+++ .projenrc.py
@@ -5,7 +5,7 @@ from projen.awscdk import (
     AwsCdkPythonApp,
 )
 
-__version__ = "1.0.8"
+__version__ = "1.0.9"
 project = AwsCdkPythonApp(
     auto_merge=False,
     author_email="meronian@amazon.ch",
